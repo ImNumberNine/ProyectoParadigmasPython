84c2c201443b6cbe97338b5427b93387af2eafd2
ImNumberNine <91976820+ImNumberNine@users.noreply.github.com>
2023-11-22 12:12:35 -0600

    Análisis estadistico de las canciones a través de categorías como lo bailables que son, si son muy instrumentales, etc. Nuevas funcionalidades en la interfaz para que lo mencionado acerca del EDA se muestre en pantalla

--- ProyectoFinalParadigmas/SpotifyAppLogica.py
+++ ProyectoFinalParadigmas/SpotifyAppLogica.py
@@ -4,14 +4,10 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-
-# Autenticación con Spotify API
 def authenticate_spotify(client_id, client_secret):
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
-# Función para obtener las características de las canciones más populares
 def get_top_tracks_features(sp, playlist_id):
     top_tracks = sp.playlist_tracks(playlist_id)
     tracks_info = []
@@ -21,91 +17,45 @@ def get_top_tracks_features(sp, playlist_id):
         track = track_entry['track']
         track_ids.append(track['id'])
         tracks_info.append({
-            'id': track['id'],  # Este es el ID de la canción que Spotify asigna
+            'id': track['id'],
             'name': track['name'],
             'artist': ', '.join([artist['name'] for artist in track['artists']])
         })
 
     features = sp.audio_features(track_ids)
-    return tracks_info, features
+    for i, feature in enumerate(features):
+        if feature:  # Verifica si existe la característica
+            tracks_info[i].update(feature)
 
+    return pd.DataFrame(tracks_info)  # Devuelve un DataFrame
 
-# Función que procesa los datos y aplica el clustering
-def process_and_cluster_data(tracks_info, features, num_clusters):
-    df_tracks_info = pd.DataFrame(tracks_info)
-    df_features = pd.DataFrame(features)
-
-    # Fusionar información de pistas y características en un DataFrame
-    df = pd.merge(df_tracks_info, df_features, on='id')
-
-    # Seleccionar solo algunas características para el análisis
-    features_to_use = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness',
-                       'valence']
-    df_selected_features = df[features_to_use]
-
-    # Normalizar los datos
+def process_and_cluster_data(df, num_clusters):
+    features_to_use = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']
+    df_features = df[features_to_use]
     scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(df_selected_features)
+    scaled_features = scaler.fit_transform(df_features)
+    kmeans = KMeans(n_clusters=num_clusters, n_init=10, random_state=42)
 
-    # Implementar KMeans Clustering
-    kmeans = KMeans(n_clusters=3, n_init=10, random_state=42)
     kmeans.fit(scaled_features)
-
-    # Añadir la columna de clusters al DataFrame original
     df['cluster'] = kmeans.labels_
-
-    return df[['id', 'name', 'artist', 'cluster']]
-
-
+    return df
 
 def get_recommendations(clustered_data, song_id, num_recommendations=5):
-    """
-    Genera recomendaciones de canciones basadas en el cluster de una canción específica.
-
-    :param clustered_data: DataFrame con las canciones y sus clusters.
-    :param song_id: El ID de la canción que el usuario ha seleccionado.
-    :param num_recommendations: Número de recomendaciones a generar.
-    :return: DataFrame con las canciones recomendadas.
-    """
-    # Encuentra el cluster de la canción seleccionada
     song_cluster = clustered_data[clustered_data['id'] == song_id]['cluster'].values[0]
-
-    # Filtra canciones del mismo cluster
     same_cluster_songs = clustered_data[clustered_data['cluster'] == song_cluster]
-
-    # Elimina la canción seleccionada de las recomendaciones
     recommendations = same_cluster_songs[same_cluster_songs['id'] != song_id]
-
-    # Si hay demasiadas recomendaciones, devuelve una muestra aleatoria
     if len(recommendations) > num_recommendations:
         recommendations = recommendations.sample(n=num_recommendations)
-
-    return recommendations[['name', 'artist', 'id', 'cluster']]
-
-
-import pandas as pd
+    return recommendations
 
 def export_cluster_results_to_csv(clustered_data, file_path):
-    """
-    Exporta los resultados del clustering a un archivo CSV.
-
-    :param clustered_data: DataFrame con los resultados del clustering.
-    :param file_path: Ruta del archivo CSV de destino.
-    """
     try:
         clustered_data.to_csv(file_path, index=False)
     except Exception as e:
         print(f"Error exporting to CSV: {e}")
 
 def export_cluster_results_to_excel(clustered_data, file_path):
-    """
-    Exporta los resultados del clustering a un archivo Excel.
-
-    :param clustered_data: DataFrame con los resultados del clustering.
-    :param file_path: Ruta del archivo Excel de destino.
-    """
     try:
         clustered_data.to_excel(file_path, index=False)
     except Exception as e:
         print(f"Error exporting to Excel: {e}")
-
--- ProyectoFinalParadigmas/main.py
+++ ProyectoFinalParadigmas/main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import io
+import seaborn as sns
 import base64
 from PIL import Image
 import PySimpleGUI as sg
@@ -9,92 +10,111 @@ from SpotifyAppLogica import (
     export_cluster_results_to_csv, export_cluster_results_to_excel
 )
 
-# Configura tus credenciales de Spotify
+# Configuración de las credenciales de Spotify
 client_id = 'ce28579afae84f4281e87ae12658f1c0'
 client_secret = '2dacb21be0814ff2ab4654e0401be8de'
 sp = authenticate_spotify(client_id, client_secret)
 
-# Establece un tema para la GUI
+# DataFrame para almacenar datos
+df = pd.DataFrame()
+
+# Configuración del tema para la interfaz gráfica
 sg.theme('DarkTeal9')
 
-# Estructura de la sección de carga de datos
+# Definición de las secciones de la interfaz
 load_section = [
     [sg.Text('Playlist ID:', size=(15, 1)), sg.InputText(key='PLAYLIST_ID')],
     [sg.Button('Load Data', size=(10, 1))]
 ]
 
-# Estructura de la sección de clustering y visualización de resultados
 cluster_section = [
     [sg.Text('Number of Clusters:', size=(15, 1)), sg.Slider(range=(2, 10), orientation='h', size=(34, 20), default_value=3, key='NUM_CLUSTERS')],
     [sg.Button('Cluster', size=(10, 1))],
     [sg.Table(values=[], headings=['ID', 'Name', 'Artist', 'Cluster'], auto_size_columns=True, display_row_numbers=False, key='TABLE', size=(None, 10))]
 ]
 
-# Estructura de la sección de recomendaciones
 recommendations_section = [
     [sg.Text('Song ID for Recommendations:', size=(25, 1)), sg.InputText(key='SONG_ID')],
     [sg.Button('Get Recommendations', size=(15, 1)), sg.Listbox(values=[], size=(60, 6), key='RECOMMENDATIONS')]
 ]
 
-# Estructura de la sección de exportación
 export_section = [
     [sg.Button('Export to CSV', size=(15, 1)), sg.Button('Export to Excel', size=(15, 1))]
 ]
 
-# Layout principal que combina todas las secciones
+eda_section = [
+    [sg.Text('Select Feature for EDA:'), sg.Combo(['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence'], key='EDA_FEATURE')],
+    [sg.Button('Show Stats', size=(10, 1)), sg.Button('Show Plot', size=(10, 1))],
+    [sg.Image(key='EDA_PLOT')],
+    [sg.Multiline(key='EDA_STATS', size=(60, 5), disabled=True)]
+]
+
+# Combinación de todas las secciones en el layout principal
 layout = [
     [sg.Text('Spotify Data Analysis', font=('Helvetica', 16), justification='center', pad=(0,10))],
     [sg.Frame('Load Data', load_section, font='Any 12', title_color='yellow')],
     [sg.Frame('Cluster and Results', cluster_section, font='Any 12', title_color='yellow')],
     [sg.Frame('Recommendations', recommendations_section, font='Any 12', title_color='yellow')],
-    [sg.Frame('Export', export_section, font='Any 12', title_color='yellow')]
+    [sg.Frame('Export', export_section, font='Any 12', title_color='yellow')],
+    [sg.Frame('Exploratory Data Analysis', eda_section, font='Any 12', title_color='yellow')]
 ]
 
-# Crea la ventana
+# Creación de la ventana
 window = sg.Window('Spotify Data Analysis', layout, resizable=True)
 
-# Inicializa las variables para almacenar los datos de la playlist y las características
-tracks_info, features, clustered_data = None, None, pd.DataFrame()
+# Función para convertir un gráfico de matplotlib en una imagen para PySimpleGUI
+def convert_plot_to_image(figure):
+    with io.BytesIO() as output:
+        figure.savefig(output, format="PNG")
+        plt.close(figure)
+        return base64.b64encode(output.getvalue())
 
-# Event loop para procesar "eventos" y obtener los "valores" de las entradas
+# Bucle de eventos para manejar la interacción del usuario
 while True:
     event, values = window.read()
     if event in (None, 'Exit'):
         break
 
-    elif event == 'Load Data':
+    if event == 'Load Data':
         playlist_id = values['PLAYLIST_ID']
-        sp = authenticate_spotify(client_id, client_secret)
-        tracks_info, features = get_top_tracks_features(sp, playlist_id)
-        # Actualiza la tabla con los datos cargados, incluyendo el ID de la canción
-        table_data = [[track['id'], track['name'], track['artist']] for track in tracks_info]
-        window['TABLE'].update(values=table_data)
-        clustered_data = pd.DataFrame()  # Actualiza clustered_data
+        df = get_top_tracks_features(sp, playlist_id)
 
     elif event == 'Cluster':
-        num_clusters = values['NUM_CLUSTERS']
-        if tracks_info and features:
-            clustered_data = process_and_cluster_data(tracks_info, features, num_clusters)
-            # Actualiza la tabla con los resultados del clustering, incluyendo el ID de la canción
+        if not df.empty:
+            num_clusters = int(values['NUM_CLUSTERS'])
+            clustered_data = process_and_cluster_data(df, num_clusters)
             table_data = [[row['id'], row['name'], row['artist'], row['cluster']] for index, row in clustered_data.iterrows()]
             window['TABLE'].update(values=table_data)
+
     elif event == 'Get Recommendations':
-        song_id = values['SONG_ID']
-        if not clustered_data.empty and song_id:
+        if not clustered_data.empty:
+            song_id = values['SONG_ID']
             recommended_songs = get_recommendations(clustered_data, song_id)
-            # Convierte las recomendaciones en una lista para mostrar en la GUI
             recommendations_to_show = [' - '.join([row['id'], row['name'], row['artist']]) for index, row in recommended_songs.iterrows()]
             window['RECOMMENDATIONS'].update(recommendations_to_show)
+
     elif event == 'Export to CSV':
         if not clustered_data.empty:
             file_path = sg.popup_get_file('Choose a CSV file to save the results', save_as=True, default_extension=".csv")
-            if file_path:
-                export_cluster_results_to_csv(clustered_data, file_path)
+            export_cluster_results_to_csv(clustered_data, file_path)
+
     elif event == 'Export to Excel':
         if not clustered_data.empty:
             file_path = sg.popup_get_file('Choose an Excel file to save the results', save_as=True, default_extension=".xlsx")
-            if file_path:
-                export_cluster_results_to_excel(clustered_data, file_path)
+            export_cluster_results_to_excel(clustered_data, file_path)
+
+    elif event == 'Show Stats':
+        if not df.empty:
+            selected_feature = values['EDA_FEATURE']
+            stats = df[selected_feature].describe().to_string()
+            window['EDA_STATS'].update(stats)
+
+    elif event == 'Show Plot':
+        if not df.empty:
+            selected_feature = values['EDA_FEATURE']
+            fig = plt.figure()
+            sns.histplot(df[selected_feature], kde=True)
+            image = convert_plot_to_image(fig)
+            window['EDA_PLOT'].update(data=image)
 
 window.close()
-
